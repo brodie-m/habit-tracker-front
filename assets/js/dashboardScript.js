6523d3db91a9d7ce3857af0370f92d623ee16e4a
brodie-m <brodiedmcguire@gmail.com>
2021-10-04 22:02:17 +0100

    added token check to dashboard

--- assets/js/dashboardScript.js
+++ assets/js/dashboardScript.js
@@ -4,12 +4,13 @@ window.addEventListener('load', async ()=> {
     const options = {
         "method": "POST",
         "headers": {
+            'Content-Type': 'application/json',
             "auth-token": token || registerToken 
         }
     }
     const result = await fetch('http://localhost:3000/api/user/verify',options)
-    const message = await result.json().message;
-    console.log(message)
+    const message = await result.json();
+    if(message.message !== "good token") {window.location.href = "./index.html"}
 })
 
 
