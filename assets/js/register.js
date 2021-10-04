eb8b09cdf75e1085a7f2242479ee17d2f6954a9c
dimtaiwo <dimejitaiwo@Dimejis-MacBook-Pro.local>
2021-10-04 17:16:48 +0100

    Login and resgiter route displaying error from database

--- assets/js/register.js
+++ assets/js/register.js
@@ -18,56 +18,54 @@
 
 // This function displays the errors on the form to warn the user
 const displayError = (errors) => {
-    // Getting the errors element to append the errors
-    const errorsElement = document.querySelector(".register-errors");
+  // Getting the errors element to append the errors
+  const errorsElement = document.querySelector(".register-errors");
 
-    // Clearing previous errors
-    errorsElement.textContent = "";
+  // Clearing previous errors
+  errorsElement.textContent = "";
 
-    for (const error of errors) {
-        const element = document.createElement("p");
-        element.textContent = error;
-        element.style.color = "red";
-        element.style.textAlign = "center";
+  for (const error of errors) {
+    const element = document.createElement("p");
+    element.textContent = error;
+    element.style.color = "red";
+    element.style.textAlign = "center";
 
-        // Appending the error
-        errorsElement.appendChild(element);
-    }
+    // Appending the error
+    errorsElement.appendChild(element);
+  }
 };
 
 // This function validates the username
 const validateUsername = (username) => {
-    // Removing leading and trailing whitespaces from the username
-    // Should be removed on form submission too
-    const cleanUsername = username.trim();
+  // Removing leading and trailing whitespaces from the username
+  // Should be removed on form submission too
+  const cleanUsername = username.trim();
 
-    // Only alpha-numeric characters are allowed
-    const re = /^[a-zA-Z0-9]+$/;
+  // Only alpha-numeric characters are allowed
+  const re = /^[a-zA-Z0-9]+$/;
 
-    const errors = [];
+  const errors = [];
 
-    if (!re.test(String(cleanUsername).toLowerCase()))
-        errors.push("Username must be alphanumeric");
+  if (!re.test(String(cleanUsername).toLowerCase()))
+    errors.push("Username must be alphanumeric");
 
-    if (cleanUsername.length < 6)
-        errors.push("Username must be at least 6 characters");
+  if (cleanUsername.length < 6)
+    errors.push("Username must be at least 6 characters");
 
-    return errors;
+  return errors;
 };
 
 // This function validates the password
 const validatePassword = (password, confirmedPassword) => {
-    const errors = [];
+  const errors = [];
 
-    if (password != confirmedPassword)
-        errors.push("Password does not match");
+  if (password != confirmedPassword) errors.push("Password does not match");
 
-    if (password.length < 6)
-        errors.push("Password must be at least 6 characters");
+  if (password.length < 6)
+    errors.push("Password must be at least 6 characters");
 
-    return errors;
+  return errors;
 };
-
 
 /**
  * USER REGISTRATION
@@ -78,49 +76,57 @@ const registerButton = document.getElementById("register");
 
 // This function handles the registration process
 const registerHandler = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    // Getting the values from the form
-    const registerUsername = document.getElementById("register-username").value;
-    const registerPassword = document.getElementById("register-password").value;
-    const registerConfirmPassword = document.getElementById("register-confirm-password").value;
-    const registerEmail = document.getElementById("register-email").value
-    // Errors will be added here to give the user useful messages
-    const errors = [];
+  // Getting the values from the form
+  const registerUsername = document.getElementById("register-username").value;
+  const registerPassword = document.getElementById("register-password").value;
+  const registerConfirmPassword = document.getElementById(
+    "register-confirm-password"
+  ).value;
+  const registerEmail = document.getElementById("register-email").value;
+  // Errors will be added here to give the user useful messages
+  const errors = [];
 
-    // Check if the username is valid and add errors if there are any
-    errors.push(...validateUsername(registerUsername));
+  // Check if the username is valid and add errors if there are any
+  errors.push(...validateUsername(registerUsername));
 
-    // Check if the password is valid and add errors if there are any
-    errors.push(...validatePassword(registerPassword, registerConfirmPassword));
+  // Check if the password is valid and add errors if there are any
+  errors.push(...validatePassword(registerPassword, registerConfirmPassword));
 
-    // If errors is empty, inputs are valid and can proceed with API call
-    // Otherwise, print errors on page
-    if (!(errors.length == 0)) {
-        displayError(errors)
-        return;
-    }
-    const options = {
-        'method': 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({
-            "name": registerUsername,
-            "email": registerEmail,
-            "password": registerPassword
+  // If errors is empty, inputs are valid and can proceed with API call
+  // Otherwise, print errors on page
+  if (!(errors.length == 0)) {
+    displayError(errors);
+    return;
+  }
+  const options = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      name: registerUsername,
+      email: registerEmail,
+      password: registerPassword,
+    }),
+  };
+  const result = await fetch(
+    "http://localhost:3000/api/user/register",
+    options
+  );
+  console.log(result);
+  if (result.status !== 201 || result.status === 400) {
+    const errorResult = await result.json();
+    displayError([errorResult.error]);
+    console.log(errorResult.error);
+    return;
+  }
+  console.log("success");
+  const data = await result.json();
+  console.log(data);
+  localStorage.setItem("registerToken", data.token);
+  window.location.href = "./dashboard.html";
 
-        })
-    }
-    const result = await fetch("http://localhost:3000/api/user/register", options)
-    if (result.status!==200 || result.status === 400) {
-        const errorResult = await result.json()
-        console.log(errorResult.error)
-        return
-    }
-    console.log('success')
-    console.log(await result.json())
-
-    // API call here
-
+  // API call here
 };
 
 // Adding the event handler to the listener
