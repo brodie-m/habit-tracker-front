51cbbffbc29ee8c4da38b3bb376933d1913bf65a
brodie-m <brodiedmcguire@gmail.com>
2021-10-06 09:16:47 +0100

    dashboard redirect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+window.addEventListener("load", async () => {
+  const token = localStorage.getItem("token");
+  const registerToken = localStorage.getItem("registerToken");
+  const options = {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json",
+          "auth-token": token || registerToken,
+      },
+  };
+  const result = await fetch("http://localhost:3000/api/user/verify", options);
+  const message = await result.json();
+  if (message.message === "good token") {
+      window.location.href = "./dashboard.html";
+  }
+});
+
+
 // Grabbing the login and registration button from the landing page
 const landingLoginButton = document.getElementById("login-button");
 const landingRegisterButton = document.getElementById("register-button");
