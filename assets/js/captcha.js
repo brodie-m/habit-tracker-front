df88a214bc24ec4fec17356791aa7df1eee34e01
Arafath Miah <miah.arafath@gmail.com>
2021-10-06 18:17:51 +0100

    added more captchas

--- assets/js/captcha.js
+++ assets/js/captcha.js
@@ -1,7 +1,16 @@
 const captchas = {
     1: "2b4m",
     2: "wy3e",
-    3: "4sx8"
+    3: "4sx8",
+    4: "hsl7",
+    5: "5fwv",
+    6: "zd3s",
+    7: "h1y2",
+    8: "3fzz",
+    9: "ts4a",
+    10: "11x1",
+    11: "yffc",
+    12: "5uwv"
 }
 
 const getRandomCaptcha = () => {
