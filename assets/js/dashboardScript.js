a30579b7817046d546850b9973e5ec905c09ee30
Arafath Miah <miah.arafath@gmail.com>
2021-10-07 10:41:45 +0100

    fire is moving

--- assets/js/dashboardScript.js
+++ assets/js/dashboardScript.js
@@ -22,7 +22,7 @@ window.addEventListener("load", async () => {
     if (message.message !== "good token") {
         window.location.href = "./index.html";
     } else {
-        localStorage.setItem('user-id',message._id)
+        localStorage.setItem('user-id', message._id)
         console.log('calling getHabits()')
         await getHabits();
     }
@@ -128,7 +128,7 @@ async function drawHabits(result) {
 
         //create fire div
         const fireThing = document.createElement('div')
-        fireThing.classList.add('fire-moving')
+        // fireThing.classList.add('fire-moving')
         if (habit.frequency.daily === true) {
             fireThing.classList.add('fire-daily')
         }
@@ -138,6 +138,37 @@ async function drawHabits(result) {
         if (habit.frequency.monthly === true) {
             fireThing.classList.add('fire-monthly')
         }
+
+        fireThing.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 185.68 238.8">
+                <defs>
+                <style>.cls-1{fill:#FC7226;}.cls-2{fill:#FC8223;}.cls-3{fill:#FC8223;}.cls-4{fill:#FC9823;}</style>
+                </defs>
+            
+                <g class="cir" data-name="Layer 5">
+                    <circle class="cls-1 cirGroup1" cx="59.04" cy="98.87" r="3.48"/>
+                    
+                    <circle id="my-circle" class="cls-1 cirGroup2" cx="35.53" cy="189.04" r="6.31"/>
+                    <circle class="cls-1 cirGroup1" cx="145.7" cy="197.82" r="6.31"/>
+                    <circle id="my-circle1" class="cls-1 cirGroup2" cx="159.7" cy="173.67" r="3.47"/>
+                    <circle class="cls-1 cirGroup3" cx="48.51" cy="159.07" r="3.47"/>
+                    <circle class="cls-1 cirGroup3" cx="141.07" cy="137.56" r="2.45"/>
+                    <circle id="my-circle2" class="cls-1 cirGroup2" cx="100.67" cy="131.2" r="2.45"/>
+                    
+                </g>
+            
+                <g data-name="Layer 3">
+                    <!--   sideFlames   -->
+                    <path class="cls-2 sideFlame1" d="M47.38,151c-9.76-4-34.9-17.48-34.9-17.48s2.29,46,15,68.12l.06.1q.33.58.68,1.13l.25.41a30.73,30.73,0,0,0,3.93,5.06l.18.17a34,34,0,0,0,46.91,2.17c14.19-12.33,16.39-34.45,3.38-48C71,150.36,57.14,155.07,47.38,151Z"/>
+                    <path class="cls-2 sideFlame" d="M136.58,149c9.76-4,34.9-17.48,34.9-17.48s-2.29,46-15,68.12l-.06.1q-.33.58-.68,1.13l-.25.41a30.73,30.73,0,0,1-3.93,5.06l-.18.17a34,34,0,0,1-46.91,2.17c-14.19-12.33-16.39-34.45-3.38-48C112.91,148.36,126.82,153.07,136.58,149Z"/> 
+                    
+                    <!--   MainFlames   -->
+                    <path class="cls-1 flame" d="M158.83,151.17C145.08,107.52,91.48,42.72,91.48,42.72S39.85,107.18,25.64,148.44a60.27,60.27,0,0,0-5.34,24.82c0,20.82,10.67,39.35,27.26,51.29l.59.42,1.26.87A75.2,75.2,0,0,0,92,238.8c39.32,0,71.19-29.19,71.19-65.2A60.19,60.19,0,0,0,158.83,151.17Z"/>       
+                    <path class="cls-3 flame3" d="M147.49,159.2c-11.43-39.6-56-98.38-56-98.38s-42.9,58.47-54.71,95.91a59.16,59.16,0,0,0,18.21,69l.49.38,1,.79a59.16,59.16,0,0,0,90.93-67.74Z"/>
+                    <path class="cls-4 flame2" d="M141.34,174.68c-10.17-31.89-49.79-79.23-49.79-79.23s-38.17,47.09-48.68,77.23a43.59,43.59,0,0,0-4,18.13c0,15.21,7.89,28.75,20.15,37.47l.43.31.93.64a56.06,56.06,0,0,0,31.48,9.47c29.06,0,52.63-21.33,52.63-47.63A43.52,43.52,0,0,0,141.34,174.68Z"/>
+                </g>
+            </svg>
+        `;
 
         //create progress div
         const progress = document.createElement('div')
@@ -157,7 +188,7 @@ async function drawHabits(result) {
         const incrementText = document.createElement('input')
         incrementText.setAttribute('type', 'number')
         incrementText.setAttribute('habit-id', `${habitId}`)
-        incrementText.setAttribute('index',index)
+        incrementText.setAttribute('index', index)
         incrementText.classList.add('increment-text')
         const incrementMinus = document.createElement('i')
         incrementMinus.classList.add('fas', 'fa-minus', 'minus-button')
@@ -187,8 +218,8 @@ async function drawHabits(result) {
         //append 
 
         taskHolder.appendChild(newTask)
-        topTask.style.background = `linear-gradient(90deg, rgba(${255-completionFrac*255},50,${completionFrac*255},0.3) ${completionFrac * 100 - 5}%, rgba(${255-completionFrac*255},50,${completionFrac*255},0.3)  ${completionFrac * 100}%, rgba(244,244,246,1) ${completionFrac * 100 + 1}%, rgba(244,244,246,1) 100% )`
-        
+        topTask.style.background = `linear-gradient(90deg, rgba(${255 - completionFrac * 255},50,${completionFrac * 255},0.3) ${completionFrac * 100 - 5}%, rgba(${255 - completionFrac * 255},50,${completionFrac * 255},0.3)  ${completionFrac * 100}%, rgba(244,244,246,1) ${completionFrac * 100 + 1}%, rgba(244,244,246,1) 100% )`
+
 
         // background: linear-gradient(90deg, rgba(0,170,184,1) 0%, rgba(73,192,203,1) 90%, rgba(244,244,246,1) 100%, rgba(244,244,246,1) 100%);
         topTask.appendChild(newCircle)
@@ -238,9 +269,9 @@ async function drawHabits(result) {
     const incrementTexts = document.getElementsByClassName('increment-text')
     for (const input of incrementTexts) {
         input.addEventListener('keydown', incrementHandler)
-            
-        } 
-    
+
+    }
+
 }
 
 async function letsgo() {
@@ -312,9 +343,9 @@ function buildGraph() {
         .attr(
             "d",
             d3
-            .line()
-            .x((d) => xScale(d[0]))
-            .y((d) => yScale(d[1]))
+                .line()
+                .x((d) => xScale(d[0]))
+                .y((d) => yScale(d[1]))
         )
         .attr("transform", "translate(100, 50)")
         .attr("stroke", "black")
@@ -326,9 +357,9 @@ beav.addEventListener("mouseover", () => {
     beav.src = "./assets/images/mascot-eyes-closed-happy.png";
     clearInterval(blink);
 });
- 
+
 const theToken = localStorage.getItem('token') || localStorage.getItem('registerToken')
-const userData =  fetch("https://fp-habitab.herokuapp.com/api",{
+const userData = fetch("https://fp-habitab.herokuapp.com/api", {
     "method": 'GET',
     "headers": {
         "auth-token": theToken
@@ -499,7 +530,7 @@ async function submitHabitHandler(event) {
 
 const deleteHabitButton = document.getElementById('delete-button')
 deleteHabitButton.addEventListener("click", async (e) => {
-    
+
     e.preventDefault();
     const habitNameSpaces = document.getElementById("edit-habit-name").value;
     const habitNameUnderscores = habitNameSpaces.split(' ').join('_')
@@ -510,7 +541,7 @@ deleteHabitButton.addEventListener("click", async (e) => {
         headers: {
             "auth-token": token,
         }
-        
+
     }
     await fetch(`https://fp-habitab.herokuapp.com/api/habits/delete/${habitNameUnderscores}`, options)
     window.location.href = "./dashboard.html";
@@ -533,7 +564,7 @@ function circleHandler(e) {
 }
 
 async function incrementHandler(e) {
-    if(e.keyCode !== 13) {
+    if (e.keyCode !== 13) {
         return
     }
     const id = this.getAttribute("habit-id");
@@ -546,8 +577,8 @@ async function incrementHandler(e) {
     this.value = ""
     const newFrac = newCurrent / targetVal;
     progressText.textContent = `Progress: ${newCurrent}/${targetVal}`
-    taskBox.style = `background: linear-gradient(90deg, rgba(${255-newFrac*255},50,${newFrac*255},0.3) ${newFrac * 100 - 5
-        }%, rgba(${255-newFrac*255},50,${newFrac*255},0.3) ${newFrac * 100}%, rgba(244,244,246,1) ${newFrac * 100 + 1
+    taskBox.style = `background: linear-gradient(90deg, rgba(${255 - newFrac * 255},50,${newFrac * 255},0.3) ${newFrac * 100 - 5
+        }%, rgba(${255 - newFrac * 255},50,${newFrac * 255},0.3) ${newFrac * 100}%, rgba(244,244,246,1) ${newFrac * 100 + 1
         }%, rgba(244,244,246,1) 100% )`;
     taskBox.setAttribute("currentVal", `${newCurrent}`);
     const options = {
@@ -567,7 +598,7 @@ async function incrementHandler(e) {
         `https://fp-habitab.herokuapp.com/api/habits/updatecurrent/${index}`,
         options
     );
-    
+
 }
 
 async function plusHandler(e) {
@@ -583,8 +614,8 @@ async function plusHandler(e) {
     const newCurrent = parseInt(currentVal) + 1;
     const newFrac = newCurrent / targetVal;
     progressText.textContent = `Progress: ${newCurrent}/${targetVal}`
-    taskBox.style = `background: linear-gradient(90deg, rgba(${255-newFrac*255},50,${newFrac*255},0.3) ${newFrac * 100 - 5
-        }%, rgba(${255-newFrac*255},50,${newFrac*255},0.3) ${newFrac * 100}%, rgba(244,244,246,1) ${newFrac * 100 + 1
+    taskBox.style = `background: linear-gradient(90deg, rgba(${255 - newFrac * 255},50,${newFrac * 255},0.3) ${newFrac * 100 - 5
+        }%, rgba(${255 - newFrac * 255},50,${newFrac * 255},0.3) ${newFrac * 100}%, rgba(244,244,246,1) ${newFrac * 100 + 1
         }%, rgba(244,244,246,1) 100% )`;
     taskBox.setAttribute("currentVal", `${newCurrent}`);
     //update server afterwards
@@ -618,9 +649,9 @@ async function minusHandler(e) {
     const newCurrent = parseInt(currentVal) - 1;
     const newFrac = newCurrent / targetVal;
     progressText.textContent = `Progress: ${newCurrent}/${targetVal}`
-    taskBox.style = `background: linear-gradient(90deg, rgba(${255-newFrac*255},50,${newFrac*255},0.3) ${newFrac * 100 - 5
-    }%, rgba(${255-newFrac*255},50,${newFrac*255},0.3) ${newFrac * 100}%, rgba(244,244,246,1) ${newFrac * 100 + 1
-    }%, rgba(244,244,246,1) 100% )`;
+    taskBox.style = `background: linear-gradient(90deg, rgba(${255 - newFrac * 255},50,${newFrac * 255},0.3) ${newFrac * 100 - 5
+        }%, rgba(${255 - newFrac * 255},50,${newFrac * 255},0.3) ${newFrac * 100}%, rgba(244,244,246,1) ${newFrac * 100 + 1
+        }%, rgba(244,244,246,1) 100% )`;
     taskBox.setAttribute("currentVal", `${newCurrent}`);
 
     const options = {
@@ -732,18 +763,18 @@ async function displaySingleHabit(_id) {
     const bestStreak = habitObj.completion.daysComplete.join('').split('0')
     let freqValue;
     let slicer;
-        for (const [key, value] of Object.entries(habitObj.frequency)) {
-            if (value === true) {
-                freqValue = key
-            }
+    for (const [key, value] of Object.entries(habitObj.frequency)) {
+        if (value === true) {
+            freqValue = key
         }
-    if(freqValue === 'daily') {slicer = 1}
-    if(freqValue === 'weekly') {slicer = 7}
-    if(freqValue === 'monthly') {slicer = 30}
+    }
+    if (freqValue === 'daily') { slicer = 1 }
+    if (freqValue === 'weekly') { slicer = 7 }
+    if (freqValue === 'monthly') { slicer = 30 }
     let max = 0
     for (streak of bestStreak) {
         if (streak.length > max) {
-            max = Math.floor(streak.length/slicer)
+            max = Math.floor(streak.length / slicer)
         }
     }
     bestStreakNumber.textContent = max
